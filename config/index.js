42fdcb2e1bf662c9089c7e6754e433148a31dce1
marcialwushu <marcialwushu@gmail.com>
2018-10-17 17:56:07 -0300

    token generate

---
+++ config/index.js
@@ -0,0 +1,5 @@
+const config = {}
+
+config.JWT_KEY = 'devdeck101'
+
+module.exports = config;
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//jwt
+var jwt = require('jsonwebtoken');
+
 /**
  * Receive email and password
  * Find user 
@@ -8,6 +11,24 @@ var router = express.Router();
  */
 router.post('/login', (req, res, next) => {
     const { email, password } = req.body.userData;
+
+    if( email === undefined || password === undefined){
+        res.status(401).json({
+            success: false,
+            code: 'DD101_API_ERROR_01',
+            message: "E-mail e/or password invalid"
+        });
+    } else {
+        let tokenData = {
+            id: 101
+        }
+        let generatedToken = jwt.sign(tokenData, 'somepass', { expiresIn: '1m' });
+        res.json({
+            success: true,
+            token: generatedToken
+        })
+    }
+
 
     res.json({
         sendEmail: email
