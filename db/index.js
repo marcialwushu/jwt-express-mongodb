ce6b415575ed827665933fcc6b345491d3425728
marcialwushu <marcialwushu@gmail.com>
2018-10-18 13:44:28 -0300

    db folder with MongoClient connection

--- db/index.js
+++ db/index.js
@@ -1,3 +1,15 @@
 var config = require('../config');
 var MongoClient = require('mongodb').MongoClient;
 
+
+var db;
+var collection;
+MongoClient.connect(config.MONGO_URL, (err, database) => {
+    if(!err){
+        console.log('Connection established to MongoDB');
+        db = database;
+        collection = db.collection('users');
+    } else {
+        console.log('Not possible to established the connection to MongoDB')
+    }
+})
