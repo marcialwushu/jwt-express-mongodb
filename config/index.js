834931fdd44ba3936db083c749607df7f20194cc
marcialwushu <marcialwushu@gmail.com>
2018-10-24 16:34:27 -0200

    Not possible to established the connection to MongoDB POST /users/register 500 15.006 ms - 1634 Error: No default engine was specified and no extension was provided.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {}
 
 config.JWT_KEY = 'devdeck101';
-config.MONGO_URL = 'mongodb://localhost:27017/devdeck101';
+config.MONGO_URL = 'mongodb://localhost:60584/devdeck101';
 
 module.exports = config;
--- db/index.js
+++ db/index.js
@@ -5,21 +5,21 @@ var MongoClient = require('mongodb').MongoClient;
 var db;
 var collection;
 MongoClient.connect(config.MONGO_URL, { useNewUrlParser: true }, (err, database) => {
-    // if(!err){
-    //     console.log('Connection established to MongoDB');
-    //     db = database;
-    //     //collection = db.collection('users');
-    // } else {
-    //     console.log('Not possible to established the connection to MongoDB')
+    if(!err){
+        console.log('Connection established to MongoDB');
+        db = database;
+        //collection = db.collection('users');
+    } else {
+        console.log('Not possible to established the connection to MongoDB')
         
-    // }
-    const client = (async() => {
+    }
+    // const client = (async() => {
 
-        await MongoClient.connect(config.MONGO_URL, { useNewUrlParser: true });
-        const db = client.db();
-        await db.dropDatabase();
+    //     await MongoClient.connect(config.MONGO_URL, { useNewUrlParser: true });
+    //     const db = client.db();
+    //     await db.dropDatabase();
     
-    })();
+    // })();
     
     
  });
