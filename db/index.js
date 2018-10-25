1816c8bba48d3e8f89a1f239d5fe70865c5dedf8
marcialwushu <marcialwushu@gmail.com>
2018-10-25 17:18:07 -0200

    throw err null MongoClient connection

--- db/index.js
+++ db/index.js
@@ -2,17 +2,27 @@ var config = require('../config');
 var MongoClient = require('mongodb').MongoClient;
 
 
-var db;
-var collection;
+//var db;
+//var collection;
 MongoClient.connect(config.MONGO_URL, { useNewUrlParser: true }, (err, database) => {
-    if(!err){
-        console.log('Connection established to MongoDB');
-        db = database;
-        //collection = db.collection('users');
-    } else {
-        console.log('Not possible to established the connection to MongoDB')
+    if(!err) throw err;
+
+    var db = database.db('devdeck101');
+
+    db.collection('users').findOne({}, function (findErr, result) {
+        if (findErr) throw findErr;
+        console.log(result.name);
+        database.close();
+    });
+
+
+    //     console.log('Connection established to MongoDB');
+    //     db = database;
+    //     collection = db.collection('users');
+    // return console.log('Not possible to established the connection to MongoDB');
         
-    }
+        
+    
     // const client = (async() => {
 
     //     await MongoClient.connect(config.MONGO_URL, { useNewUrlParser: true });
